d24ff2043b7febb2b642697779cfa4ff73bc926d
Luciano <lucianodefranca@outlook.com>
2024-09-15 16:54:32 -0300

    feat: add stuff

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,10 +10,26 @@ variable "service_cpu" {}
 
 variable "service_memory" {}
 
+variable "service_healthcheck" {}
+
 variable "ssm_vpc_id" {}
 
 variable "ssm_listener" {}
 
 variable "ssm_priv_subnet_1" {}
+
 variable "ssm_priv_subnet_2" {}
+
 variable "ssm_priv_subnet_3" {}
+
+variable "environment_variables" {
+  type = list
+}
+
+variable "capabilities" {
+  type = list
+}
+
+variable "service_launch_type" {}
+
+variable "service_task_count" {}
